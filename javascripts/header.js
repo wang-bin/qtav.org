591414ab0f8520fd9b728de7b1f61eb64c703b63
wang-bin <wbsecg1@gmail.com>
2015-05-21 23:50:16 +0800

    add missing js

---
+++ javascripts/header.js
@@ -0,0 +1,12 @@
+(function(window, undefined) {
+  document.write('<div id="header"><nav>'
+    + '<li class="fork"><a href="https://github.com/wang-bin/QtAV">Fork On GitHub</a></li>'
+    + '<li class="fork"><a href="https://github.com/wang-bin/QtAV/wiki/_pages">Wiki</a></li>'
+    + '<li class="fork"><a href="https://github.com/wang-bin/QtAV/issues">Issues</a></li>'
+    + '<li class="downloads"><a href="https://sourceforge.net/projects/qtav/files/ci">Latest</a></li>'
+    + '<li class="downloads"><a href="https://sourceforge.net/projects/qtav/files/release">Release</a></li>'
+    + '<li class="title" data-l10n-id="DOWNLOADS">DOWNLOADS</li>'
+    + '<li class="title" data-l10n-id="QtAV">QtAV</li>'
+  + '</nav></div>');
+
+})(window);
