924026a5b0cf489dd9fe36ab38e3633bd05b4147
wang-bin <wbsecg1@gmail.com>
2016-01-02 03:40:19 +0800

    misc

--- javascripts/header.js
+++ javascripts/header.js
@@ -3,7 +3,7 @@
     + '<li class="fork"><a href="https://github.com/wang-bin/QtAV"><span class="fa fa-github"></span> Github</a></li>'
     + '<li class="fork"><a href="https://github.com/wang-bin/QtAV/wiki/_pages"><span class="fa fa-book"></span> Wiki</a></li>'
     + '<li class="fork"><a href="https://github.com/wang-bin/QtAV/issues"><span class="fa fa-bug"></span> Bug</a></li>'
-    + '<li class="fork"><a href="http://wang-bin.github.io/QtAV"><span class="fa fa-home"></span> Home</a></li>'
+    + '<li class="fork"><a href="http://qtav.org"><span class="fa fa-home"></span> Home</a></li>'
     + '<li class="downloads"><a href="http://www.qtav.org/install.html"><span class="fa fa-cloud-download"></span> <span data-l10n-id="Install">Install</span></a></li>'
     + '<li class="title" data-l10n-id="QtAV" style="font-size:18px;">QtAV</li>'
   + '</nav></div>');
